90f8acdd8459ea6bfbcbecec407e239ebe9a0146
zjhou <z@zjh.im>
2021-02-07 22:44:51 +0800

    feat: list of latest article's title

--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -5,6 +5,7 @@ import { Await } from '../../components/await';
 import { get } from '../../utils/req';
 import { apiURL } from '../../constants';
 import CLS from './index.scss';
+import { PcOnly } from "../../components/pc_only";
 
 export const Article = (props) => {
   return (
@@ -17,14 +18,43 @@ export const Article = (props) => {
   )
 }
 
+const scrollToArticle = (articleTitle) => {
+  const $article = document.querySelector(`[data-title="${articleTitle}"]`);
+  $article.scrollIntoView({
+    block: 'start',
+  })
+}
+
+const LatestArticleTitles = (props) => {
+  const { articles = [] } = props;
+  const titles = articles.slice(0, 10).map(a => a.title);
+  return (
+    <PcOnly>
+      <ul className={CLS.latestArticlesTitles}>
+        {titles.map(t => (
+          <li
+            onClick={() => {
+              scrollToArticle(t);
+            }}
+            className={CLS.latestArticlesTitle}
+          >{t}</li>
+        ))}
+      </ul>
+    </PcOnly>
+  )
+};
+
 export const Articles = (props) => {
   return (
     <Await promise={get(apiURL.articles)}>
       {(articles) => {
         return (
-          <div className={cn(CLS.articles, props.className)}>
-            {articles.map((article) => <Article {...article} />)}
-          </div>
+          <>
+            <LatestArticleTitles articles={articles} />
+            <div className={cn(CLS.articles, props.className)}>
+              {articles.map((article) => <Article {...article} />)}
+            </div>
+          </>
         );
       }}
     </Await>
