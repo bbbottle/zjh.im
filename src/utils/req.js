b1813675113b7503dd1e22c28db3cb15d68d06c6
zjhou <z@zjh.im>
2021-02-26 23:18:55 +0800

    refactor: remove unused req utils

--- src/utils/req.js
+++ src/utils/req.js
@@ -1,33 +1,3 @@
-import React, { useState, useEffect } from 'react';
-import { ajax as ajaxMod } from 'rxjs';
-
-const { ajax } = ajaxMod;
-
-export const get = (url) => {
-  const res$ = ajax.getJSON(url);
-  return new Promise((res) => {
-    return res$.subscribe(res)
-  })
-}
-
-export const Get = (props) => {
-  const [loading, setLoading] = useState(true);
-  const [res, setRes] = useState(null);
-  const { url, children } = props;
-  useEffect(() => {
-    get(url)
-      .then((res) => {
-        setRes(res);
-        setLoading(false)
-      })
-  }, []);
-
-  return children({
-    loading,
-    data: res,
-  })
-};
-
 export const preloadImg = (imgSrc) => {
   let img = new Image();
   img.src = imgSrc;
